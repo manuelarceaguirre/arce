e74a02609609e5fa672232b977d09705c294408e
Manuel Arce <manuel.arceaguirre@gmail.com>
2025-08-22 19:31:56 -0700

    daily update

--- statistics/practice.R
+++ statistics/practice.R
@@ -1,2 +1,16 @@
-# uniformity -> if we have a subset of numbers from 0 to 1 N we should have an expected value of n/N
-# independence -> the probability of one doesn't need to depend on the past one
+# Biased coin simulation: 10 batches × 50 tosses (p(Head)=0.7)
+set.seed(20250819)
+n_batches  <- 10
+batch_size <- 50
+p_head     <- 0.7
+
+# Generate all 500 tosses at once as 0/1 (1 = Head)
+tosses <- rbinom(n_batches * batch_size, size = 1, prob = p_head)
+
+# Reshape into a 50×10 matrix - CHANGE: byrow = FALSE
+M <- matrix(tosses, nrow = batch_size, ncol = n_batches, byrow = FALSE)
+heads_per_batch <- colSums(M)
+
+# Show the results
+heads_per_batch
+cat(paste(heads_per_batch, collapse = ", "), "\n")
